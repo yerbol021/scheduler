73e13530111ae85dbc9ba5941afc28260a3a824c
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-24 17:57:25 -0700

    refactoring useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
 export default function useVisualMode(initial) {
-  return { mode: initial };
+  const [mode, setMode] = useState(initial);
+
+  return { mode };
 }

3a1050c8f9814c03fcc87769fe12276586fdea2b
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-25 00:10:00 -0700

    implementing limits

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,8 +3,12 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); 
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode])
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, -1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
@@ -12,7 +16,6 @@ export default function useVisualMode(initial) {
       setHistory(prev => prev.slice(0, -1));
     }
   }
-
   return { mode: history[history.length -1], transition, back }
 
 }

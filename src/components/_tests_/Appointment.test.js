d46d602f920ef1b6836738914c9cc5d9d4013578
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-26 20:00:04 -0700

    working on tests. more test coverage%

--- src/components/_tests_/Appointment.test.js
+++ src/components/_tests_/Appointment.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { waitForElement } from "@testing-library/react";
 import Appointment from "../Appointment";
+import Application from "components/Application";
+import { fireEvent } from "@testing-library/react";
 
 describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });

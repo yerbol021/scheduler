9ae5504d97c90205c5e4dd3b9e112ed4a8edcebf
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-28 00:36:57 -0700

    add test which should cancel an interview

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,15 +1,24 @@
 describe("Navigation", () => {
-  
   it("should visit root", () => {
     cy.visit("/");
   });
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-  
+
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.class", "day-list__item--selected")
+      .should("have.class", "day-list__item--selected");
   });
 
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
 });
